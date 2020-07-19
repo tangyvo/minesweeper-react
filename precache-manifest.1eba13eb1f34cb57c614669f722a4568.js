a1305eecd97f95029ef861c4c7e8e4dae21b63f0
Yvonne <tangy222@gmail.com>
2020-07-19 22:15:07 +0100

    Updates

---
+++ precache-manifest.1eba13eb1f34cb57c614669f722a4568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c947f3ef28ba034a23a59947a3e6e15",
+    "url": "/minesweeper-react/index.html"
+  },
+  {
+    "revision": "533dcef27a336fcdd83e",
+    "url": "/minesweeper-react/static/css/main.4b94421b.chunk.css"
+  },
+  {
+    "revision": "b9c7c0f39c276f7f6116",
+    "url": "/minesweeper-react/static/js/2.f8c4b755.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/minesweeper-react/static/js/2.f8c4b755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533dcef27a336fcdd83e",
+    "url": "/minesweeper-react/static/js/main.be26cb98.chunk.js"
+  },
+  {
+    "revision": "89d07356abf0bd97b3c3",
+    "url": "/minesweeper-react/static/js/runtime-main.d7dcd411.js"
+  }
+]);
